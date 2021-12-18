f91b8bb3d57185a270f822f571bc943f54420c19
Guilherme <guilhermehscolpani@gmail.com>
2021-12-18 12:18:05 -0300

    Upload do projeto

---
+++ README.md
@@ -0,0 +1,33 @@
+<p align="center">
+   <img src="src/assets/logo.svg" >
+</p>
+
+<h3>
+  <p align="center"> Aplicação para Gestão de Receitas e Despesas </p>
+</h3>  
+
+<p align="center">Projeto desenvolvido durante as aulas do Chapter II da trilha de ReactJS do Bootcamp Ignite da Rocketseat</p>
+
+<p align="center">
+   <img src="src/assets/home.png" >
+</p>
+
+<p align="center">
+   <img src="src/assets/modal.png" >
+</p>
+
+### Como rodar o Projeto?
+
+ * Faça o clone do repositório em seu computador e instale as dependências com o seguinte comando no terminal/bash:
+ ```bash
+ yarn
+ ```
+  * Após finalizar a instalação dos pacotes execute o seguinte comando:
+ ```bash
+yarn start
+ ```
+ * Abra em seu navegador e digite [http://localhost:3000](http://localhost:3000) para vizualizar a aplicação.
+
+
+
+
---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "../src/hooks/useTransactions";
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+      <GlobalStyle/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Dashboard/>
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+    </TransactionsProvider>
+  );
+}
